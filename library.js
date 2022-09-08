65b21d62f79e7d475139a3551f4705398822fb5b
Tuan <tuanmai1996@gmail.com>
2022-09-07 21:56:45 -0400

    added Book constructor and addBookToLibrary function

--- library.js
+++ library.js
@@ -1,40 +1,47 @@
-const book1 = {
-    author: "John Smith",
-    title: "Scary",
-    pages: 100,
-    read: true
-}
+// const books = {
+//     book1 : {
+//         author: "John Smith",
+//         title: "Scary",
+//         pages: 100,
+//         read: true
+//     },
 
-const book2 = {
-    author: "John Smith",
-    title: "Scary",
-    pages: 100,
-    read: true
-}
+//     book2 : {
+//         author: "George Smith",
+//         title: "Chill",
+//         pages: 99,
+//         read: true
+//     },
+    
+//     book3 : {
+//         author: "John Jones",
+//         title: "Romance",
+//         pages: 50,
+//         read: false
+//     }
+// };
+const bookAuthor = document.getElementById('author');
+const bookTitle = document.getElementById('title');
+const bookPages = document.getElementById('pages');
 
-const book3 = {
-    author: "John Smith",
-    title: "Scary",
-    pages: 100,
-    read: true
-}
+let myLibrary = [];
 
-
-
-let myLibrary = [book1, book2, book3];
-
-function Book() {
-  // the constructor...
+function Book(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
-  
-}
-
-function display(myLibrary) {
-    for (book in myLibrary) {
-        console.log(book.author);
-    } 
-}
+    if (bookAuthor.value.length === 0 || bookTitle.value.length === 0) {
+      alert("Please, fill all the fields");
+      return;
+    }
+    const newBook = new Book(bookAuthor, bookTitle, bookPages); 
+    myLibrary.push(newBook);
+  }
+// for (book in books) {
+//     myLibrary.push(books[book]);
+// }
 
 
